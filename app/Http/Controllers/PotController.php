aeb24ddb12bccc9c9286852331d61e27e4fa23e6
runonce <camilorivera86@gmail.com>
2017-10-04 23:42:48 -0300

    Initial commit Installed Laravel 5.5 and created the controllers and models files for Boxes and Pots.

---
+++ app/Http/Controllers/PotController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pot;
+use Illuminate\Http\Request;
+
+class PotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pot  $pot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pot $pot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pot  $pot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pot $pot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pot  $pot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pot $pot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pot  $pot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pot $pot)
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_10_05_015023_create_boxes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boxes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->smallInteger('temperature')->nullable();
+            $table->smallInteger('humidity')->nullable();
+            $table->smallInteger('water')->nullable();
+            $table->boolean('lights')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boxes');
+    }
+}
