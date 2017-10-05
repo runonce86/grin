a4afaf0e7f5f7b2f939d2d1da55d3140c53f611c
runonce <camilorivera86@gmail.com>
2017-10-05 19:29:07 -0300

    Renemed Watering Cans migration

--- database/migrations/2017_10_05_016023_create_watering_cans_table.php
+++ database/migrations/2017_10_05_016023_create_watering_cans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateWaterCansTable extends Migration
+class CreateWateringCansTable extends Migration
 {
     /**
      * Run the migrations.
