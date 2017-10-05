d10dc0c521f1019c4dc415959db21edf15b07aad
runonce <camilorivera86@gmail.com>
2017-10-04 23:56:04 -0300

    Added relation between the pots table and the boxes table

--- database/migrations/2017_10_05_015057_create_pots_table.php
+++ database/migrations/2017_10_05_015057_create_pots_table.php
@@ -15,9 +15,12 @@ class CreatePotsTable extends Migration
     {
         Schema::create('pots', function (Blueprint $table) {
             $table->increments('id');
+            $table->smallInteger('box_id')->nullable();
             $table->string('name')->nullable();
             $table->smallInteger('humidity')->nullable();
             $table->timestamps();
+
+            $table->foreign('box_id')->references('id')->on('boxes');
         });
     }
 
