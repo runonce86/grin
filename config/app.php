5513ce52bcd12978bd24be130baa2a946b664878
runonce <camilorivera86@gmail.com>
2017-10-16 21:38:51 -0300

    Installed jwt-auth

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Application Service Providers...
